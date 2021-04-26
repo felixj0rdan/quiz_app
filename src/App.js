8c4a6675c5d02572aed04d21ed1c987d4acdf31e
Felix Jordan <felixjorda312@gmail.com>
2021-04-26 11:48:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React,{useState,useEffect} from 'react';
 import Quiz from './Quiz';
+import './App.css';
+
+
 const api = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
 
 
 function App() {
     const [data,setData] = useState([]);
+    const [theme,setTheme] = useState(false);
+    const darkTheme = {
+      backgroundColor:"black",
+      color:"white",
+      // marginTop: 20
+      // padding:50
+    }
+    const lightTheme = {
+      backgroundColor:"white",
+      color:"black",
+      // marginTop: 10
+
+      // padding:50
+    }
+    const toggle = () => {
+      setTheme(!theme)
+    }
+
     useEffect(()=>{
         fetch(api)
             .then(response => response.json())
@@ -13,10 +34,15 @@ function App() {
     },[])
     var n=0,s=-1
     return (
-        <div>
+        <div style={theme ? darkTheme : lightTheme}>
+            {/* <button type="submit" onClick={toggle}>Toggle Theme</button> */}
+            <label className="switch">
+            <input type="checkbox" onChange={toggle} />
+            <span className="slider round"></span>
+            </label>
             {data.map(data => (
               <div>
-                <Quiz ques={data.question} ans={data.correct_answer} opts={data.incorrect_answers} no={n+=1} start= {s+=4}/> 
+                <Quiz theme={theme ? darkTheme : lightTheme} ques={data.question} ans={data.correct_answer} opts={data.incorrect_answers} no={n+=1} start= {s+=4}/> 
               </div>
             ))}
         </div>
--- src/Quiz.js
+++ src/Quiz.js
@@ -2,13 +2,16 @@ import './Quiz.css'
 import React, { useState, useEffect } from 'react'
 
 
-export default function Quiz({ques, ans, opts, no, start}) {
+
+export default function Quiz({ques, ans, opts, no, start,theme}) {
     opts = opts.concat(ans)
     opts.sort()
-    var message=""
+    var message="", message_cl=""
     var opt=["btn-primary","btn-primary","btn-primary","btn-primary"]
-    const [check, setCheck] = useState()
-    const [mess, setMess] = useState()
+    const [check, setCheck] = useState();
+    const [mess, setMess] = useState();
+    const [score,setScore] = useState(0);
+    // let score = 0;
     var pos
     if(check) {
         opt=["btn-danger","btn-danger","btn-danger","btn-danger"]
@@ -18,10 +21,16 @@ export default function Quiz({ques, ans, opts, no, start}) {
                 opt[i]="btn-success"
                 pos=i;
             }
-            if (pos === mess)
-                message="Right"
-            else
-                message="Wrong"     
+            if (pos === mess){
+                message="Right";
+                message_cl="messageR";
+            }
+                
+                
+            else{
+                message="Wrong";
+                message_cl="messageL";
+            }     
     }
     function dest(ad) {
         var ids = [0,1,2,3] 
@@ -29,17 +38,21 @@ export default function Quiz({ques, ans, opts, no, start}) {
             
             document.getElementById(ids[i]+ad).disabled =true;
             console.log(ids[i]+ad);
+
         }
     }
+ 
     return (
-        <div className="card">
-            <div className="Ques">{no}. {ques}</div>
+        <div className="card" style={theme}>
+         
+            <div className="Ques" style={{color: theme.color}}>{no}. {ques}</div>
             <br></br>
             <button id={start+0} className={opt[0]} onClick={() => {setCheck(true); setMess(0)}}>{opts[0]}</button><br/>
             <button id={start+1} className={opt[1]} onClick={() => {setCheck(true); setMess(1)}}>{opts[1]}</button><br/>
             <button id={start+2} className={opt[2]} onClick={() => {setCheck(true); setMess(2)}}>{opts[2]}</button><br/>
             <button id={start+3} className={opt[3]} onClick={() => {setCheck(true); setMess(3)}}>{opts[3]}</button><br/>
-            <div className="message">{message}</div>
+            <div className={message_cl}>{message}</div>
+            <div>{score}</div>
             <br></br>
         </div>
     );
